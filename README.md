e99c0b5f4e85f743be18950a18d728babcd18dab
root <root@serverweb.com>
2020-03-11 17:16:40 +0100

    Add

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-#Drive
+This is a simple private Drive. It born for store personal files.
+Is made up of severals web languages such as HTML, CSS, JavaScript and PHP.
+How do it:
+1) Dowload the repository --> clone https://github.com/P1c1s/drive.git
+2) After the download enter in drive directory --> cd drive
+3) Boot the installer as Root or Suodoers --> sudo ./installer.sh
+4) Open browser and go to ip of machine e.g. 192.168.1.x/drive
--- installer.sh
+++ installer.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 clear;
 echo -e '\033[0;31m';
-echo '+-------------------------------------------------------------------------------------+';
-echo '                                                                               _____'
-echo ' ____________    ___________         ____________   _______    ______     _____\    \';
-echo '\           \   \          \       /            \  \      |  |      |   /    / |    |';
-echo ' \           \   \    /\    \     |\___/\  \\___/|  |     /  /     /|  /    /  /___/|';
-echo  '  |    /\     |   |   \_\    |     \|____\  \___|/  |\    \  \    |/  |    |__ |___|/';
-echo '  |   |  |    |   |      ___/            |  |       \ \    \ |    |   |       \';
-echo '  |    \/     |   |      \  ____    __  /   / __     \|     \|    |   |     __/ __';
-echo '  /           /|  /     /\ \/    \  /  \/   /_/  |     |\         /|   |\    \  /  \';
-echo ' /___________/ | /_____/ |\______| |____________/|     | \_______/ |   | \____\/    |';
-echo '|           | /  |     | | |     | |           | /      \ |     | /    | |    |____/|';
-echo '|___________|/   |_____|/ \|_____| |___________|/        \|_____|/      \|____|   | |';
-echo '                                                                              |___|/';
-echo '+-------------------------------------------------------------------------------------+';
+echo '+--------------------------------------+';
+echo '|    _____          _                  |';
+echo '|   (____ \        (_)                 |';
+echo '|    _   \ \  ____  _  _   _  ____     |';
+echo '|   | |   | |/ ___)| || | | |/ _  )    |';
+echo '|   | |__/ /| |    | | \ V /( (/ /     |';
+echo '|   |_____/ |_|    |_|  \_/  \____)    |';
+echo '|                                      |';
+echo '+------------'$(date +%a-%d-%M-%Y)'------------+';
 
 
 echo -e '\033[1;37m';
@@ -28,10 +23,10 @@ if [ "$decision" = "y" ]; then
    #Upadating
    apt-get update &&  apt-get upgrade
    #Installation of Apache2
-   apt-get install apache2 && apt-get install php7.3 && apt-get install libapache2-mod-php7.3
+   apt-get install apache2 -y && apt-get install php7.3 -y && apt-get install libapache2-mod-php7.3 -y
 
    #Move drive to web-root, Remove installer and Show ip
    mv ../drive /var/www/html/drive
    rm /var/www/html/drive/installer.sh
-   echo -e '\033[0;31m Go to ' $(hostname -I)
+   echo -e '\033[0;31m Go to ' $(hostname -I)/drive
 fi
