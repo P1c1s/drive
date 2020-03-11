d362abd4a2eb9003c57d35798c252b86bf51c17a
root <root@serverweb.com>
2020-03-11 12:23:45 +0100

    INSTALLER.SH

---
+++ installer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+clear;
+echo -e '\033[0;31m';
+echo '+-------------------------------------------------------------------------------------+';
+echo '                                                                               _____'
+echo ' ____________    ___________         ____________   _______    ______     _____\    \';
+echo '\           \   \          \       /            \  \      |  |      |   /    / |    |';
+echo ' \           \   \    /\    \     |\___/\  \\___/|  |     /  /     /|  /    /  /___/|';
+echo  '  |    /\     |   |   \_\    |     \|____\  \___|/  |\    \  \    |/  |    |__ |___|/';
+echo '  |   |  |    |   |      ___/            |  |       \ \    \ |    |   |       \';
+echo '  |    \/     |   |      \  ____    __  /   / __     \|     \|    |   |     __/ __';
+echo '  /           /|  /     /\ \/    \  /  \/   /_/  |     |\         /|   |\    \  /  \';
+echo ' /___________/ | /_____/ |\______| |____________/|     | \_______/ |   | \____\/    |';
+echo '|           | /  |     | | |     | |           | /      \ |     | /    | |    |____/|';
+echo '|___________|/   |_____|/ \|_____| |___________|/        \|_____|/      \|____|   | |';
+echo '                                                                              |___|/';
+echo '+-------------------------------------------------------------------------------------+';
+
+
+echo -e '\033[1;37m';
+
+echo "Hi, I am a the installer of Drive. You must execute me like root or sudoers."
+read -p "Do you want install me? YES [y] NO [n]: " decision
+
+   echo 'Go to ' $(hostname -I)
+
+if [ "$decision" = "y" ]; then
+
+   #Upadating
+   sudo apt-get update &&  sudo apt-get upgrade
+   #Installation of Apache2
+   sudo apt-get install apache2 && sudo apt-get install libapache2-mod-php
+
+   mv ../drive-master /var/www/html/drive
+   echo 'Go to ' hostanme -I
+   rm /var/www/html/drive/installer.sh
+fi
