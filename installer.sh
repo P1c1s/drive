89758562dfd9d2607417f8cc7d1b123a14ac2f55
root <root@serverweb.com>
2020-03-11 17:39:03 +0100

    upadate-intsaller

--- installer.sh
+++ installer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 clear;
-echo -e '\033[0;31m';
-echo '+--------------------------------------+';
-echo '|    _____          _                  |';
-echo '|   (____ \        (_)                 |';
-echo '|    _   \ \  ____  _  _   _  ____     |';
-echo '|   | |   | |/ ___)| || | | |/ _  )    |';
-echo '|   | |__/ /| |    | | \ V /( (/ /     |';
-echo '|   |_____/ |_|    |_|  \_/  \____)    |';
-echo '|                                      |';
-echo '+------------'$(date +%a-%d-%M-%Y)'------------+';
+echo -e '\033[0;37m+\033[0;31m--------------------------------------\033[0;37m+\033[0;31m';
+echo -e '|   \033[0;37m _____\033[0;31m          \033[1;37m _\033[0;31m                 |';
+echo -e '|   \033[0;37m(____ \ \033[0;31m        \033[1;37m(_)\033[0;31m                |';
+echo -e '|   \033[0;37m _   \ \ \033[0;31m  ____ \033[1;37m _\033[0;31m  _   _ \033[0;37m ____\033[0;31m    |';
+echo -e '|   \033[0;37m| |  | | \033[0;31m|/ ___)\033[1;37m| |\033[0;31m| | | |\033[0;37m/ _  )\033[0;31m   |';
+echo -e '|   \033[0;37m| |__/ / \033[0;31m| |    \033[1;37m| |\033[0;31m \ V /\033[0;37m( (/ /\033[0;31m    |';
+echo -e '|   \033[0;37m|_____/ \033[0;31m |_|   \033[1;37m |_|\033[0;31m  \_/ \033[0;37m \____)\033[0;31m   |';
+echo -e '|                                      |';
+echo -e '\033[0;37m+\033[0;31m------------\033[0;35m'$(date +%a-%d-%M-%Y)'\033[0;31m------------\033[0;37m+';
 
 
 echo -e '\033[1;37m';
@@ -25,8 +24,11 @@ if [ "$decision" = "y" ]; then
    #Installation of Apache2
    apt-get install apache2 -y && apt-get install php7.3 -y && apt-get install libapache2-mod-php7.3 -y
 
-   #Move drive to web-root, Remove installer and Show ip
+   #Move drive to web-root, Change PErmission, Remove installer and Show ip
    mv ../drive /var/www/html/drive
+   chown www-data /var/www/html/files -R && chgrp www-data /var/www/html/drive/files -R
+   chown www-data /var/www/html/trash -R && chgrp www-data /var/www/html/drive/trash -R
    rm /var/www/html/drive/installer.sh
+   echo '';
    echo -e '\033[0;31m Go to ' $(hostname -I)/drive
 fi
